b1cced5904a3d4cbe5f5bfec0be5b147c63cfa65
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2024-11-26 22:55:11 -0500

    Integración de la clase MascotasService

--- src/main/java/com/proyectojpa/proyectojpa/service/MascotaService.java
+++ src/main/java/com/proyectojpa/proyectojpa/service/MascotaService.java
@@ -1,7 +1,35 @@
 package com.proyectojpa.proyectojpa.service;
 
+import com.proyectojpa.proyectojpa.model.Mascota;
+
+import java.util.List;
+
 public class MascotaService implements IMascotaService{
 
 private IMascotaService iMascotaService;
 
+    @Override
+    public List<Mascota> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Mascota findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void save(Mascota mascota) {
+
+    }
+
+    @Override
+    public void delete(Mascota mascota) {
+
+    }
+
+    @Override
+    public void update(Mascota mascota) {
+
+    }
 }
