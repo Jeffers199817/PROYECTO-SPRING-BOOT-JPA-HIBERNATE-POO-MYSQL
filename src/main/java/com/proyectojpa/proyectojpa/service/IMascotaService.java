98bc5ab342f8a1e563f82dc90fa76c5ce6883101
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2024-11-26 22:54:22 -0500

    Integración de los metodos crud

--- src/main/java/com/proyectojpa/proyectojpa/service/IMascotaService.java
+++ src/main/java/com/proyectojpa/proyectojpa/service/IMascotaService.java
@@ -1,4 +1,21 @@
 package com.proyectojpa.proyectojpa.service;
 
+import com.proyectojpa.proyectojpa.model.Mascota;
+
+import java.util.List;
+
 public interface IMascotaService {
+
+
+public List<Mascota> findAll();
+
+public Mascota findById(int id);
+
+public void save(Mascota mascota);
+
+public void delete(Mascota mascota);
+
+public void update(Mascota mascota);
+
+
 }
